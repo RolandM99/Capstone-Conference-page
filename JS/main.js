afb4a281584665b19a2ee6ad39f7113cfb7a1900
rolandm99 <manfulmweze99@gmail.com>
2021-10-26 07:12:46 +0100

    Add more and hide button

--- JS/main.js
+++ JS/main.js
@@ -25,7 +25,7 @@ const ourTeam = [
     image: 'images/mireille.jpg',
     name: 'Mireille Muhigwa',
     degree: 'Msc. International Devellopment and Globalisation',
-    about: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    about: 'Mireille MUHIGWA works to empower women in her native country DRC.She serves as the founder and CEO of Strong Women DRC.',
   },
   {
     image: 'images/profil2.jpg',
@@ -45,7 +45,7 @@ const ourTeam = [
     image: 'images/profil4.jpg',
     name: 'Dr. Denis MUkwege',
     degree: 'Nobel Price 2018',
-    about: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    about: 'Dr. Denis Mukwege is our first partner, with his project BADILIKA we work together to empower women victims of gender based violence in their local activities.',
   },
   {
     image: 'images/profil5.jpg',
@@ -56,21 +56,39 @@ const ourTeam = [
   },
 ];
 
-const dataTeam = (n) => {
+ourTeam.forEach((member) => {
   sectionTeam.innerHTML += ` <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card2">
-      <img src="${ourTeam[n].image}" class="card-img-top" alt="profil one">
+      <img src="${member.image}" class="card-img-top" alt="profil one">
       <div class="card-body">
-        <h2 class="card-name">${ourTeam[n].name}</h2>
-        <p class="card-text1">${ourTeam[n].degree}</p>
+        <h2 class="card-name">${member.name}</h2>
+        <p class="card-text1">${member.degree}</p>
         <hr class="line-txt">
-          <p class="card-text2">${ourTeam[n].about}</p>
+          <p class="card-text2">${member.about}</p>
       </div>
     </div>
   </div>`;
-};
+});
 
-for (let i = 0; i < ourTeam.length; i += 1) {
-  dataTeam(i);
-}
+const teamMember = document.querySelectorAll('.row');
+const btn = document.querySelector('#team > div > div.see-more > a');
+let state = true;
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (state) {
+    Array.from(teamMember).forEach((member) => {
+      member.classList.add('show');
+      btn.innerHTML = '<span>Hide</span><span><i class="fa fa-chevron-up"></i></span>';
+    });
+    state = false;
+  } else {
+    state = false;
+    Array.from(teamMember).forEach((member) => {
+      member.classList.remove('show');
+      btn.innerHTML = '<span>More</span><span><i class="fa fa-chevron-down"></i></span>';
+    });
+    state = true;
+  }
+});
